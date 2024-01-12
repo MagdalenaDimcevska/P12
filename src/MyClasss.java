d51dd2c0ff8566b4d26572d66a51103caa6353da
MagdalenaDimcevska <147504217+MagdalenaDimcevska@users.noreply.github.com>
2024-01-12 16:17:28 +0100

    Add first java project

---
+++ src/MyClasss.java
@@ -0,0 +1,8 @@
+public class MyClasss {
+	public static void main(String[] args) {
+		Vehicle1 v1=new Vehicle1("Red");
+		Vehicle1 v2=new Vehicle1 ();
+		System.out.println(v1);
+		System.out.println(v2);
+	}
+}
---
+++ src/Vehicle1.java
@@ -0,0 +1,14 @@
+public class Vehicle1 {
+    private String color;
+
+    Vehicle1(String a) {
+        color = a;
+    }
+    Vehicle1(){
+    	color="Blue";
+    }
+    @Override
+    public String toString() {
+        return color;
+    }
+}
